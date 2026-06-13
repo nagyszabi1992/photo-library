1ba2929c65f6e2db68f53e4b1b7ac7875a5e6644
Szabolcs Nagy <nagyszabi1992@gmail.com>
2026-06-13 10:02:05 +0200

    initial angular modernization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  changeDetection: ChangeDetectionStrategy.Eager,
-  standalone: false,
 })
 export class AppComponent {
-  title = 'gallery-template';
+  readonly title = 'Photo Library';
 }
